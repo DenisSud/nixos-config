153d0a3b348035f14dcc9bce6aa1bc60ad23e000
Denis Sudakov <85438327+DenisSud@users.noreply.github.com>
2024-05-27 04:42:14 -0400

    Update home.nix

--- home.nix
+++ home.nix
@@ -152,63 +152,13 @@ with lib.hm.wt;
     '';
   };
 
-  gtk = {
-    enable = true;
-
-    iconTheme = {
-      name = "Adwaita";
-      package = pkgs.gnome.adwaita-icon-theme;
-    };
-
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome.gnome-themes-extra;
-    };
-
-  };
-
   dconf.settings = {
-
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-
-    "org/gnome/desktop/peripherals/mouse" = {
-      accel-profile = "flat";
-    };
 
     "org/gnome/desktop/peripherals/touchpad" = {
       two-finger-scrolling-enabled = true;
     };
 
-
-
   };
-
-  home.pointerCursor =
-    let
-      getFrom = url: hash: name: {
-        gtk.enable = true;
-        x11.enable = true;
-        name = name;
-        size = 26;
-        package =
-          pkgs.runCommand "moveUp" { } ''
-            mkdir -p $out/share/icons
-            ln -s ${pkgs.fetchzip {
-              url = url;
-              hash = hash;
-            }} $out/share/icons/${name}
-          '';
-      };
-    in
-    getFrom
-      "https://github.com/manu-mannattil/adwaita-cursors/releases/download/v1.2/adwaita-cursors.tar.gz"
-      "sha256-zKa55zn4UO/cCTx2Es0xKxUwjFe5/k5xWI9RLJYpvsQ="
-      "Adwaita";
-
-
-
 
 
 
