f5288c07e60e87d9e5f4f62207e44dc3075050ef
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-13 15:16:57 +0300

    updated conifg

--- modules/rice.nix
+++ modules/rice.nix
@@ -2,7 +2,7 @@
 {
   stylix = {
     base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-seaside.yaml";
-    image = /home/denis/Pictures/space_person.jpg;
+    image = /home/denis/Pictures/green_waterfall.jpg;
 
     # cursor.package = pkgs.apple-cursor;
 
