88ffacd4b362064a196db1daca48a44a5020fc0f
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-16 18:03:00 +0300

    updated conifg

--- modules/rice.nix
+++ modules/rice.nix
@@ -1,7 +1,7 @@
 {config, pkgs, ...}:
 {
   stylix = {
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-lakeside-light.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-savanna-light.yaml";
     image = /home/denis/Pictures/dark_guy_1.jpg;
 
     # cursor.package = pkgs.apple-cursor;
