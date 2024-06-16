97770cefb22d943d5adce40b0cbbc6d4c8235563
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-16 20:11:05 +0300

    updated conifg

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -9,16 +9,10 @@
 
     nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = true;
-      powerManagement.finegrained = true;
-      open = false;
-      package = config.boot.kernelPackages.nvidiaPackages.production;
-      nvidiaSettings = true;
       prime = {
-        sync.enable = false; # enable for maximum performance
+        sync.enable = true; # enable for maximum performance
         offload = { # enable for better power managment
-			    enable = true;
-			    enableOffloadCmd = true;
+			    enable = false;
 		    };
         amdgpuBusId = "PCI:4:0:0";
         nvidiaBusId = "PCI:1:0:0";
