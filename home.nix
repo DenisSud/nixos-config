5dab0da4ad3f5bf4415cdd0e791cf2122fc6bf71
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-21 15:13:56 +0300

    updated conifg

--- home.nix
+++ home.nix
@@ -12,15 +12,160 @@
       enable = true;
     };
 
-    neovim = {
+    # neovim = {
 
+    #   viAlias = true;
+    #   vimAlias = true;
+    #   vimdiffAlias = true;
+	
+    #   enable = true;
+    #   extraConfig = ''
+     	 
+    #     vim.g.mapleader = " "
+    #     vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
+
+    #     vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+    #     vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+    #     vim.keymap.set("n", "J", "mzJ`z")
+    #     vim.keymap.set("n", "<C-d>", "<C-d>zz")
+    #     vim.keymap.set("n", "<C-u>", "<C-u>zz")
+    #     vim.keymap.set("n", "n", "nzzzv")
+    #     vim.keymap.set("n", "N", "Nzzzv")
+    #     vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+
+    #     vim.keymap.set("n", "<leader>vwm", function()
+    #         require("vim-with-me").StartVimWithMe()
+    #     end)
+    #     vim.keymap.set("n", "<leader>svwm", function()
+    #         require("vim-with-me").StopVimWithMe()
+    #     end)
+
+    #     -- greatest remap ever
+    #     vim.keymap.set("x", "<leader>p", [["_dP]])
+
+    #     -- next greatest remap ever : asbjornHaland
+    #     vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+    #     vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+    #     vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+    #     -- This is going to get me cancelled
+    #     vim.keymap.set("i", "<C-c>", "<Esc>")
+
+    #     vim.keymap.set("n", "Q", "<nop>")
+    #     vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+    #     vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+    #     vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+    #     vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+    #     vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+    #     vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+    #     vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+    #     vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+    #     vim.keymap.set(
+    #         "n",
+    #         "<leader>ee",
+    #         "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+    #     )
+
+    #     vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+    #     vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+    #     vim.keymap.set("n", "<leader><leader>", function()
+    #         vim.cmd("so")
+    #     end)
+
+    #     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    #     require("nvim-treesitter.configs").setup({
+    #         -- A list of parser names, or "all"
+    #         ensure_installed = {
+    #             "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+    #             "jsdoc", "bash", "python", "golang"
+    #         },
+
+    #         -- Install parsers synchronously (only applied to `ensure_installed`)
+    #         sync_install = false,
+
+    #         -- Automatically install missing parsers when entering buffer
+    #         -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+    #         auto_install = true,
+
+    #         indent = {
+    #             enable = true
+    #         },
+
+    #         highlight = {
+    #             -- `false` will disable the whole extension
+    #             enable = true,
+
+    #             -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    #             -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+    #             -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    #             -- Instead of true it can also be a list of languages
+    #             additional_vim_regex_highlighting = { "markdown" },
+    #         },
+    #     })
+
+    #     local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    #     treesitter_parser_config.templ = {
+    #         install_info = {
+    #             url = "https://github.com/vrischmann/tree-sitter-templ.git",
+    #             files = {"src/parser.c", "src/scanner.c"},
+    #             branch = "master",
+    #         },
+    #     }
+
+    #     vim.treesitter.language.register("templ", "templ")
+
+    #   '';
+    #   plugins = with pkgs.vimPlugins; [
+    #     nvim-lspconfig
+
+    #     comment-nvim
+        
+    #     kanagawa-nvim
+
+    #     harpoon
+
+    #     telescope-nvim 
+
+    #     telescope-fzf-native-nvim 
+
+    #     mason-nvim
+
+    #   	nvim-cmp
+
+    #     friendly-snippets
+
+    #     friendly-snippets
+
+    #   	rust-tools-nvim
+
+    #     {
+    #       plugin = (nvim-treesitter.withPlugins (p: [
+    #         p.tree-sitter-nix
+    #         p.tree-sitter-rust
+    #         p.tree-sitter-go
+    #         p.tree-sitter-vim
+    #         p.tree-sitter-bash
+    #         p.tree-sitter-lua
+    #         p.tree-sitter-python
+    #         p.tree-sitter-json
+    #       ]));
+    #   	}
+    #   ];
+    # };
+
+    neovim = {
       viAlias = true;
       vimAlias = true;
       vimdiffAlias = true;
-	
+
       enable = true;
       extraConfig = ''
-     	 
         vim.g.mapleader = " "
         vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
 
@@ -78,33 +223,18 @@
             vim.cmd("so")
         end)
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
+
         require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
                 "jsdoc", "bash", "python", "golang"
             },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
             auto_install = true,
-
-            indent = {
-                enable = true
-            },
-
+            indent = { enable = true },
             highlight = {
-                -- `false` will disable the whole extension
                 enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = { "markdown" },
             },
         })
@@ -119,31 +249,18 @@
         }
 
         vim.treesitter.language.register("templ", "templ")
-
       '';
       plugins = with pkgs.vimPlugins; [
         nvim-lspconfig
-
         comment-nvim
-        
         kanagawa-nvim
-
         harpoon
-
-        telescope-nvim 
-
-        telescope-fzf-native-nvim 
-
+        telescope-nvim
+        telescope-fzf-native-nvim
         mason-nvim
-
-	nvim-cmp
-
+        nvim-cmp
         friendly-snippets
-
-        friendly-snippets
-
-	rust-tools-nvim
-
+        rust-tools-nvim
         {
           plugin = (nvim-treesitter.withPlugins (p: [
             p.tree-sitter-nix
@@ -155,9 +272,10 @@
             p.tree-sitter-python
             p.tree-sitter-json
           ]));
-	}
+        }
       ];
-    };
+    }
+
   
     waybar = {
       enable = false;
