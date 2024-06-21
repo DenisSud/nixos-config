2d449f12d4ac966bcee113c952a692b2af5bddec
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-21 15:23:05 +0300

    updated conifg

--- home.nix
+++ home.nix
@@ -20,7 +20,7 @@
 
       enable = true;
       extraConfig = ''
-        ${builtins.readFile ./modles/vim/config.lua}
+        ${builtins.readFile /home/denis/nixos/modles/vim/config.lua}
       '';
 
       plugins = with pkgs.vimPlugins; [
