96513a3166c3cee0abcba77b18b1e779af05b040
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-23 23:42:12 +0300

    updated conifg

--- modules/vim/init.lua
+++ modules/vim/init.lua
@@ -7,53 +7,9 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
--- Packer setup
-local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
-if vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
-    vim.api.nvim_command('!git clone https://github.com/wbthomason/packer.nvim ' .. packer_install_path)
-end
-
-vim.cmd [[packadd packer.nvim]]
-local packer_ok, packer = pcall(require, 'packer')
-
-if not packer_ok then
-    return
-end
-
-return require('packer').startup(function()
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'L3MON4D3/LuaSnip'
-
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-lua/plenary.nvim'
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-
-    use 'nvim-treesitter/nvim-treesitter'
-
-    use 'NeogitOrg/neogit'
-
-    use 'kyazdani42/nvim-tree.lua'
-
-    use 'nvim-lualine/lualine.nvim'
-
-    use 'mbbill/undotree'
-    use 'ThePrimeagen/harpoon'
-    use 'tpope/vim-surround'
-    use 'tpope/vim-commentary'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'windwp/nvim-autopairs'
-    use 'numToStr/Comment.nvim'
-end)
-
 -- LSP settings
-local nvim_lsp = require('lspconfig')
-local cmp = require('cmp')
+-- local nvim_lsp = require('lspconfig')
+-- local cmp = require('cmp')
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -67,64 +23,59 @@ nvim_lsp.gopls.setup {
     capabilities = capabilities,
 }
 
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif require('luasnip').expand_or_jumpable() then
-                require('luasnip').expand_or_jump()
-            else
-                fallback()
-            end
-        end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif require('luasnip').jumpable(-1) then
-                require('luasnip').jump(-1)
-            else
-                fallback()
-            end
-        end,
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'cmdline' },
-        { name = 'luasnip' },
-    },
-}
+-- cmp.setup {
+--     mapping = {
+--         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+--         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--         ['<C-Space>'] = cmp.mapping.complete(),
+--         ['<C-e>'] = cmp.mapping.close(),
+--         ['<CR>'] = cmp.mapping.confirm {
+--             behavior = cmp.ConfirmBehavior.Replace,
+--             select = true,
+--         },
+--         ['<Tab>'] = function(fallback)
+--             if cmp.visible() then
+--                 cmp.select_next_item()
+--             elseif require('luasnip').expand_or_jumpable() then
+--                 require('luasnip').expand_or_jump()
+--             else
+--                 fallback()
+--             end
+--         end,
+--         ['<S-Tab>'] = function(fallback)
+--             if cmp.visible() then
+--                 cmp.select_prev_item()
+--             elseif require('luasnip').jumpable(-1) then
+--                 require('luasnip').jump(-1)
+--             else
+--                 fallback()
+--             end
+--         end,
+--     },
+--     sources = {
+--         { name = 'nvim_lsp' },
+--         { name = 'buffer' },
+--         { name = 'path' },
+--         { name = 'cmdline' },
+--         { name = 'luasnip' },
+--     },
+-- }
 
 -- Treesitter settings
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "python", "rust", "go", "markdown" },
-    highlight = {
-        enable = true,
-    },
-}
+-- require'nvim-treesitter.configs'.setup {
+--     ensure_installed = { "python", "rust", "go", "markdown" },
+--     highlight = {
+--         enable = true,
+--     },
+-- }
 
 -- Telescope settings
-require('telescope').setup {
-    defaults = {
-        file_ignore_patterns = { "node_modules" },
-    },
-}
-require('telescope').load_extension('fzf')
+-- require('telescope').setup {
+--     defaults = {
+--         file_ignore_patterns = { "node_modules" },
+--     },
+-- }
+-- require('telescope').load_extension('fzf')
 
 -- Neogit settings
 require('neogit').setup {}
@@ -140,13 +91,6 @@ require('lualine').setup {
         component_separators = ''
     }
 }
-
--- Additional plugins settings
-require("indent_blankline").setup {
-    show_end_of_line = true,
-}
-require('nvim-autopairs').setup {}
-require('Comment').setup {}
 
 -- Keybindings
 vim.api.nvim_set_keymap('n', '<leader>pv', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
