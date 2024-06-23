e2fa41666e809f73da716631c402b7d58c31a5d4
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-23 23:47:36 +0300

    updated conifg

--- modules/vim/init.lua
+++ modules/vim/init.lua
@@ -6,6 +6,7 @@ vim.o.wrap = false
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
+vim.g.leader = " "
 
 -- Keybindings
 vim.api.nvim_set_keymap('n', '<leader>pv', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
