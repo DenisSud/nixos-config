6ad0ecd4c42c4efa8b9f476e9125de4c65ac6e37
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-30 13:17:50 +0300

    updated conifg

--- modules/rice.nix
+++ modules/rice.nix
@@ -1,7 +1,7 @@
 {config, pkgs, ...}:
 {
   stylix = {
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-dune.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-plateau.yaml";
     image = /home/denis/Pictures/gruvbox_pacman.png;
 
     fonts = {
