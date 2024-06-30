8ac95560df7ec5c879fac967f06bf0966777073f
DenisSud <sudakov.denis.2007@gmail.com>
2024-06-30 12:38:49 +0300

    updated conifg

--- modules/vim/options.lua
+++ modules/vim/options.lua
@@ -66,6 +66,7 @@ cmp.setup {
 }
 
 -- Telescope config
+local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
