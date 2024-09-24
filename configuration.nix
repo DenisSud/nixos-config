caa7e9a1292e18e8f35822ee7a7ba51e9c253263
DenisSud <sudakov.denis.2007@gmail.com>
2024-09-24 09:24:24 +0300

    auto commit

--- configuration.nix
+++ configuration.nix
@@ -35,7 +35,7 @@
     enable = true;
     image = ./wallpaper/focus.png;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/black-metal.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
     fonts = {
       serif = {
         package = pkgs.nerdfonts;
